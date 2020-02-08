5cca0e243afe58b42574a50796d54458bf42b03a
Lexxn0x3 <35461915+Lexxn0x3@users.noreply.github.com>
2020-02-08 01:16:45 +0100

    0.0.4
    -Zoom-in/out (controler only)
    -controler support

--- Game GoDot/Player/PlayerCamera.gd
+++ Game GoDot/Player/PlayerCamera.gd
@@ -1,19 +1,29 @@
 extends Camera2D
-var zoomValue = Vector2(1, 1)
+export var zoomSpeed = 1           #exported variables
+export var zoomDefault = 0.7
+export var zoomMax = 4.1
+export var zoomMin = 0.1
+
+var zoomValue = Vector2()
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	zoomValue.x = 1
-	zoomValue.y = 1
+	zoomValue.x = zoomDefault #defaul zoom of the camera
+	zoomValue.y = zoomDefault
 
 func _physics_process(delta):
 	#get Inputs
-	#if Input.is_action_pressed"camera_zoom_in"):
-		zoomValue.x += 1
-	#if Input.is_action_pressed("camera_zoom_out"):
-	#zoomValue.x -= 1
-	#zoomValue.y = zoomValue.x
-	#if OS.is_debug_build(): print(zoomValue.x)
-	#set Zoom
-	#set_zoom(Vector2(zoomValue.x, zoomValue.y))
-	#set_zoom(zoomValue)
+	if Input.is_action_pressed("camera_zoom_out"):
+		zoomValue.x += zoomSpeed * delta * zoomValue.x * zoomValue.x
+		
+	if Input.is_action_pressed("camera_zoom_in"):
+		zoomValue.x -= zoomSpeed * delta * zoomValue.x * zoomValue.x
+
+	#if OS.is_debug_build(): print(zoomValue.x) #print debug zoom faktor
+
+	zoomValue.x = clamp(zoomValue.x, zoomMin, zoomMax)        #applay max zoom
+	zoomValue.y = zoomValue.x             #applay zoom on camera
+	set_zoom(zoomValue)
+	#is_rotating(true) 
+	#Camera2D.rotation_degrees(0.4)
