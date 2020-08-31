7d24c70e82ae3dc5139516c9c15696e7ad8ab5f7
Lexxn0x3 <35461915+Lexxn0x3@users.noreply.github.com>
2020-08-31 16:12:11 +0200

    push

---
+++ Game GoDot/Assets/Wall Torch/Torch.gd
@@ -0,0 +1,30 @@
+extends StaticBody2D
+
+export var SideTorch = false
+export var FlipHorizontal = false
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+func _enter_tree():
+	get_node("Light2D").hide()
+	if SideTorch:
+		get_node("Torch").hide()
+		get_node("Side Torch").show()
+	else:
+		get_node("Torch").show()
+		get_node("Side Torch").hide()
+	if FlipHorizontal:
+		get_node("Side Torch").flip_h = true
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_Area2D_body_entered(body):
+	get_node("Light2D").show()
